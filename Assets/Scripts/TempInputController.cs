aec7cb35b276050881dd5761abb145eafb0a98ae
Fleshmonger <fleshmonger@gmail.com>
2017-04-21 23:29:31 +0200

    Early player movement

---
+++ Assets/Scripts/TempInputController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TempInputController : MonoBehaviour
+{
+    public PlayerController player;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var direction = 0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += 1f;
+        }
+        player.MoveDirection = direction;
+    }
+}
