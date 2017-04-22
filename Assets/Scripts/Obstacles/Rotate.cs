a61315158b94f88d4423496ae0ba9c6456d364dc
DESKTOP-EBA0PTI\Mathias <matfy87@gmail.com>
2017-04-22 14:49:26 +0200

    Levels

--- Assets/Scripts/Obstacles/Rotate.cs
+++ Assets/Scripts/Obstacles/Rotate.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour {
 
+	public int speed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,6 @@ public class Rotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (Vector3.forward * Time.deltaTime * -150);
+		transform.Rotate (Vector3.forward * Time.deltaTime * speed);
 	}
 }
